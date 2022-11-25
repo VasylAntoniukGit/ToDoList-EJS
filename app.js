ec7ec3026c1401126204bd6e79e31af460c5bd36
Vasyl <v.antoniukp@gmail.com>
2022-11-25 23:21:46 +0000

    Initialisation commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); //alows us to use static files (css javascript) inside "public" folder
+// allows us to use ejs
+app.set("view engine", "ejs");
+let items = [];
+app.get("/", function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let currentDay = today.toLocaleDateString("en-US", options);
+  // engine goes to "views" folder and search for document with name "list"
+  // kindOfDay is a key it must be the same name as a name of variable inside file list ejs
+  // so how function render works: it tells to render file list.ejs inside folder views and find variable with name kindOfDay and give it value of variable day
+
+  res.render("list", { kindOfDay: currentDay, listOfItems: items });
+});
+
+// So when user click submit button variable that was added will be writed inside
+// variable item and after that make redirection to the same page but vaiable nextItem inside list esj now is not undefined
+
+app.post("/", function (req, res) {
+  item = req.body.nextItem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {      
+  console.log(`Server is runnig on port: 3000`);
+});
+
